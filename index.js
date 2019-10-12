de6166906f8b51ec25fc85e8cb2c62c8e9bde4a7
HI CRUSADE <adam9vanpersie@gmail.com>
2019-10-12 12:22:02 +0200

    new useless function

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-var Client = reqire("discord.js")
+var Discord = require("discord.js")
+var Client = new Discord.Client();
+var config = require("./Config.json")
+var modules = []
+require("dotenv").config();
+
+Client.login(process.env.token)
+
+Client.on("ready", ()  => {
+    Client.user.setActivity(`${config.game}!`, {url: null, type: 'watching'})
+    console.log(`${Client.user.tag} is now online and ready for a crusade!`)
+})
+
+Client.on("message", message => {
+
+    if(message.author.id == Client.user.id) return
+
+    if(message.content == "!ping"){
+        message.channel.send(":ping_pong: Pong!")
+    }
+})
